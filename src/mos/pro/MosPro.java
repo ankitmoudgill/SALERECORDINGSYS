007fb400823228af90761f4ea78f1b34ada16665
ankitmoudgill <112873228+ankitmoudgill@users.noreply.github.com>
2023-08-15 10:13:58 +0530

    Add files via upload

---
+++ src/mos/pro/MosPro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mos.pro;
+
+/**
+ *
+ * @author HP
+ */
+public class MosPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
